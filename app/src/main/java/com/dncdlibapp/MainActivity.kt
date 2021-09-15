f488b63094da54fe278b7d974dc1836cbc468c1f
lcl <302748574@qq.com>
2021-09-15 11:39:45 +0800

    [lcl] 修改结构。model本地引用无法关联打包问题。 方案1：将其他模块打包aar后由最终model引入然后发布到maven 方案2：所有model打包到maven后，再最终model中引用其中(因为pom文件会包含依赖关系并打包)

--- app/src/main/java/com/dncdlibapp/MainActivity.kt
+++ app/src/main/java/com/dncdlibapp/MainActivity.kt
@@ -2,6 +2,11 @@ package com.dncdlibapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.blankj.utilcode.util.ToastUtils
+import com.chat_hook.HookMethodCall
+import com.chat_hook.HookMethodCallParams
+import com.chat_hook.HookMethodHelper
+import com.chat_hook.HookMethodParams
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/dncdlibapp/app/MApp.kt
@@ -0,0 +1,32 @@
+package com.dncdlibapp.app
+
+import android.app.Application
+import com.blankj.utilcode.util.ToastUtils
+import com.chat_hook.HookMethodCall
+import com.chat_hook.HookMethodCallParams
+import com.chat_hook.HookMethodHelper
+import com.chat_hook.HookMethodParams
+import com.dncdlibapp.MainActivity
+
+/**
+ * @author lcl
+ * Date on 2021/9/14
+ * Description:
+ */
+class MApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        //添加一个hook
+        HookMethodHelper.addHookConstructorMethod(
+            HookMethodParams(
+                MainActivity::class.java,
+                null,null,object : HookMethodCall {
+                    override fun afterHookedMethod(param: HookMethodCallParams?) {
+                        ToastUtils.showShort("Hook 执行了~~~~~~")
+                    }
+                })
+        )
+    }
+
+}
